09eb59f8cffcb3745ab2bbfdcdcc32ec71326a46
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-07-27 23:18:37 +0300

    page not found page

--- client/src/Pagenotfound.js
+++ client/src/Pagenotfound.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Pagenotfound = () => {
   return (
-    <div>Pagenotfound</div>
+        <div>
+            <h1>Something went wrong. Page not found.... 404 Error</h1>
+            <p>Please visit the right URL</p>
+        </div>
   )
+
 }
 
 export default Pagenotfound
