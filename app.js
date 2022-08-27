6d5051303d58c715b2bb1f5136cc8b69f7453013
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-27 10:23:59 +0300

    new error solved

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ app.use("/api", require("./router/video"));
 // Serve static assets if in production
 
 //deploy for heroku
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //to deploy vercel
 
