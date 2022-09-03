1ee12ccecd23afbef690afa4620d499f6d0064ad
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-09-03 21:09:24 +0300

    PDF Features

---
+++ client/src/PDFFeatures/PDFFeatures.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageLayout from "../PageLayout";
+
+const PDFFeatures = () => {
+  return (
+    <PageLayout>
+      <div>PDFFeatures</div>
+    </PageLayout>
+  );
+};
+
+export default PDFFeatures;
