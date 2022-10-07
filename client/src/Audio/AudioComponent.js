733ef2e8621c9bc7922bccda670cfdbbc5fc3874
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-10-07 22:06:33 +0300

    WIP

--- client/src/Audio/AudioComponent.js
+++ client/src/Audio/AudioComponent.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import AudioStyleModule from "./AudioComponent.module.css"
+import React from "react";
+import AudioStyleModule from "./AudioComponent.module.css";
 
 const AudioComponent = () => {
   return (
     <div className={AudioStyleModule.audiocontainer}>
-
-                <h5>Title is here</h5>
-
+      <h5>Title is here</h5>
+      <p>Summery of the text</p>
+      <p>Last part of the text</p>
     </div>
-  )
-}
+  );
+};
 
-export default AudioComponent
+export default AudioComponent;
