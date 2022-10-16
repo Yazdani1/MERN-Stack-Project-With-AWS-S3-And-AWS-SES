82222c76ab05ebbdfc9572fd9c96d753249cd631
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-10-16 20:46:40 +0300

    WIP

--- client/src/List/List.js
+++ client/src/List/List.js
@@ -1,4 +1,4 @@
-import React ,{useState}from "react";
+import React, { useState } from "react";
 import listStyle from "./List.module.css";
 import PageLayout from "../PageLayout";
 
@@ -26,20 +26,54 @@ const List = () => {
     setItems([...firstItem, ...filteredItems]);
   };
 
+  const targetData = [
+    { id: false, letter: "1" },
+    { id: false, letter: "2" },
+    { id: true, letter: "3" },
+    { id: false, letter: "4" },
+    { id: true,  letter: "5" },
+    { id: false, letter: "6" },
+  ];
+
+
+  const [targetDataItem,setTargetDataItem] = useState(targetData);
+
+  const targetReorder = (id) => {
+    let firstItem;
+    const filteredItems = targetDataItem.filter((item) => {
+      if (item.id === id) {
+        firstItem = [item];
+        return false;
+      }
+      return true;
+    });
+    setTargetDataItem([...firstItem, ...filteredItems]);
+  };
+
+
   return (
     <PageLayout>
       <div className="container">
         <div className={listStyle.mainCard}>
-        
-            {items.map((item) => (
-              <div
-                onClick={() => reorder(item.id)}
-                className={listStyle.itemCard}
-              >
-                {item.letter}
-              </div>
-            ))}
-        
+          {items.map((item) => (
+            <div
+              onClick={() => reorder(item.id)}
+              className={listStyle.itemCard}
+            >
+              {item.letter}
+            </div>
+          ))}
+        </div>
+
+        <div className={listStyle.targetCard}>
+          {targetDataItem.map((item) => (
+            <div
+              onClick={() => targetReorder(item.id)}
+              className={listStyle.itemCard}
+            >
+              {item.letter}
+            </div>
+          ))}
         </div>
       </div>
     </PageLayout>
