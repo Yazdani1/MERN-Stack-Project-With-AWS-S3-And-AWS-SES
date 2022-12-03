477d1a7b5e912556d063b453e28a8631fdd775f4
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-12-03 22:10:10 +0200

    WIP

--- controller/pdffile.js
+++ controller/pdffile.js
@@ -2,9 +2,7 @@ const AWS = require("aws-sdk");
 const { uuid } = require("uuidv4");
 const { readFileSync } = require("fs");
 const PDFFile = require("../model/PDFFile");
-
 require("dotenv").config();
-
 const awsConfig = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_kEY,
@@ -12,17 +10,12 @@ const awsConfig = {
   apiVersion: process.env.AWS_API_VERSION,
   correctClockSkew: true,
 };
-
 const S3 = new AWS.S3(awsConfig);
-
 // to upload pdf file to aws server
-
 exports.uploadPdfFile = async (req, res) => {
   try {
     const { pdffile } = req.files;
-
     // console.log(video);
-
     const params = {
       Bucket: "news-note",
       Key: `${uuid()}.${pdffile.type.split("/")[1]}`,
@@ -46,27 +39,20 @@ exports.uploadPdfFile = async (req, res) => {
 exports.createPdfPost = async (req, res) => {
   try {
     const { title, pdffile } = req.body;
-
     if (!title) {
       return res.status(422).json({ error: "Please add title" });
     }
-
     if (!pdffile) {
       return res.status(422).json({ error: "Please add pdf file" });
     }
-
     const postInfo = PDFFile({ title, pdffile });
-
     const createPostwithpdf = await PDFFile.create(postInfo);
-
     return res.status(201).json(createPostwithpdf);
   } catch (error) {
     return res.status(400).json({ error: "something went wrong" });
   }
 };
-
 // to get all the pdf file
-
 exports.getAllPDFFile = async (req, res) => {
   try {
     const allpdf = await PDFFile.find({}).sort({ date: "DESC" });
@@ -79,9 +65,7 @@ exports.getAllPDFFile = async (req, res) => {
 exports.deletePdfPost = async () => {
   try {
     const delete_Query = { _id: req.params.id };
-
     const deletePdf = await PDFFile.findByIdAndDelete(delete_Query);
-
     res.status(200).json(deletePdf);
   } catch (error) {
     return res
